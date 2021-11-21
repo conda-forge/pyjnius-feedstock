ba9876df1f42b21681109674e68ba9d38bd3de81
H. Vetinari <h.vetinari@gmx.com>
2021-11-21 22:17:33 +1100

    try without activation

--- recipe/activate.bat
+++
@@ -1,4 +0,0 @@
-SET "PYJNIUS_JAR_BACKUP=%PYJNIUS_JAR%"
-SET "PYJNIUS_JAR=%CONDA_PREFIX%\share\pyjnius\pyjnius.jar"
-SET "PATH_BACKUP=%PATH%"
-SET "PATH=%JAVA_HOME%\jre\bin\server;%JAVA_HOME%\bin\server;%JAVA_HOME%\bin;%PATH%"
--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,5 @@
 @echo on
 
-setlocal EnableDelayedExpansion
-
-:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
-:: This will allow them to be run on environment activation.
-FOR %%F IN (activate deactivate) DO (
-    IF NOT EXIST %PREFIX%\etc\conda\%%F.d MKDIR %PREFIX%\etc\conda\%%F.d
-    COPY %RECIPE_DIR%\%%F.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
-    COPY %RECIPE_DIR%\%%F.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
-)
-
-call %RECIPE_DIR%\activate.bat
-
 SET PYJNIUS_SHARE=%PREFIX%\share\pyjnius
 mkdir "%PYJNIUS_SHARE%"
 
