ef2aacd4c9444dfa58126c0f5cc4dcbe5c56757f
H. Vetinari <h.vetinari@gmx.com>
2021-11-22 00:00:00 +1100

    more careful activation also on windows

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,4 +1,9 @@
-SET "CONDA_CLASSPATH_BACKUP=%CLASSPATH%"
-SET "CLASSPATH=%CLASSPATH%;%PREFIX%\share\pyjnius\pyjnius.jar"
 SET "CONDA_PATH_BACKUP=%PATH%"
 SET "PATH=%JAVA_HOME%\jre\bin\server;%JAVA_HOME%\bin\server;%JAVA_HOME%\bin;%PATH%"
+
+IF "%CLASSPATH%"=="" (
+    SET "CLASSPATH=%PREFIX%\share\pyjnius\pyjnius.jar"
+) ELSE (
+    SET "CONDA_CLASSPATH_BACKUP=%CLASSPATH%"
+    SET "CLASSPATH=%CLASSPATH%;%PREFIX%\share\pyjnius\pyjnius.jar"
+)
