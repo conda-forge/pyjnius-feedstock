4e548c49c83d65bb4096b85aabbf7450d08ac7f9
H. Vetinari <h.vetinari@gmx.com>
2021-11-21 23:52:32 +1100

    more careful activation on unix?

--- recipe/activate.sh
+++ recipe/activate.sh
@@ -1,2 +1,6 @@
-export CONDA_CLASSPATH_BACKUP="${CLASSPATH}"
-export CLASSPATH="${CLASSPATH};${PREFIX}/share/pyjnius/pyjnius.jar"
+if [ -z $CLASSPATH ]; then
+    export CLASSPATH="${PREFIX}/share/pyjnius/pyjnius.jar"
+else
+    export CONDA_CLASSPATH_BACKUP="${CLASSPATH}"
+    export CLASSPATH="${CLASSPATH};${PREFIX}/share/pyjnius/pyjnius.jar"
+fi
