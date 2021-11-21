cd5a1514c27d70d8af45850ab2568e8b6704059f
H. Vetinari <h.vetinari@gmx.com>
2021-11-22 01:33:27 +1100

    use asterisk; so sparkly

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -2,8 +2,8 @@ SET "CONDA_PATH_BACKUP=%PATH%"
 SET "PATH=%JAVA_HOME%\jre\bin\server;%JAVA_HOME%\bin\server;%JAVA_HOME%\bin;%PATH%"
 
 IF "%CLASSPATH%"=="" (
-    SET "CLASSPATH=%CONDA_PREFIX%\share\pyjnius\pyjnius.jar"
+    SET "CLASSPATH=%CONDA_PREFIX%\share\pyjnius\*"
 ) ELSE (
     SET "CONDA_CLASSPATH_BACKUP=%CLASSPATH%"
-    SET "CLASSPATH=%CLASSPATH%;%CONDA_PREFIX%\share\pyjnius\pyjnius.jar"
+    SET "CLASSPATH=%CLASSPATH%;%CONDA_PREFIX%\share\pyjnius\*"
 )
