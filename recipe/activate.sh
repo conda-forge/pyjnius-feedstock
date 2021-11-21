068b1dbb23079323f114a216dbc7fc176c9b94d9
H. Vetinari <h.vetinari@gmx.com>
2021-11-21 23:14:59 +1100

    remove patch; adapt activation; cross fingers

---
+++ recipe/activate.sh
@@ -0,0 +1,2 @@
+export CONDA_CLASSPATH_BACKUP="${CLASSPATH}"
+export CLASSPATH="${CLASSPATH};${PREFIX}/share/pyjnius/pyjnius.jar"
--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,16 @@ set -ex
 PYJNIUS_SHARE=$PREFIX/share/pyjnius
 mkdir -p $PYJNIUS_SHARE
 
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
+
 make build_ext
 
 pip install --no-deps .
-
 cp build/pyjnius.jar $PYJNIUS_SHARE
+
+jar cf pyjnius-test.jar build/test-classes
+cp pyjnius-test.jar $PYJNIUS_SHARE
