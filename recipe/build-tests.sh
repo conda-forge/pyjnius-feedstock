2c6e16d466345acaf4cd3bdf5ff8291a366877ff
H. Vetinari <h.vetinari@gmx.com>
2022-02-06 03:20:53 +1100

    add activation to pyjnius-tests

--- recipe/build-tests.sh
+++ recipe/build-tests.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}-tests.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}-tests.sh"
+done
+
 PYJNIUS_SHARE=$PREFIX/share/pyjnius
 mkdir -p $PYJNIUS_SHARE
 
