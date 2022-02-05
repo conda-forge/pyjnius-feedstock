d1655399936ead130cdcc8e66334588518acfaee
H. Vetinari <h.vetinari@gmx.com>
2022-02-06 03:18:08 +1100

    Revert "try without activation on windows"
    Interestingly, the activation would not be necessary
    for python 3.9; only failed for 3.7/3.8
    
    This reverts commit c19340fc41f5dacf88953160b4f8d4f31b4e3691.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
 @echo on
 
+:: call activation to set up JAVA_HOME correctly; the activation
+:: scripts will be copied for the pyjnius-output in build-pkg.bat
+call %RECIPE_DIR%\activate.bat
+
 call ant all
 if %ERRORLEVEL% neq 0 exit 1
 
