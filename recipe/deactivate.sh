2a298063c0f88d355edd3e16c6b57ab7d5738ab7
H. Vetinari <h.vetinari@gmx.com>
2022-02-06 18:38:20 +1100

    set CLASSPATH in activation script

--- recipe/deactivate.sh
+++ recipe/deactivate.sh
@@ -1,2 +1,8 @@
-export PYJNIUS_JAR="${PYJNIUS_JAR_BACKUP}"
-unset PYJNIUS_JAR_BACKUP
+if [[ -n "${CONDA_CLASSPATH_BACKUP}" ]]
+then
+    # only restore if CONDA_CLASSPATH_BACKUP is non-empty
+    export CLASSPATH="${CONDA_CLASSPATH_BACKUP}"
+    unset CONDA_CLASSPATH_BACKUP
+else
+    unset CLASSPATH
+fi
