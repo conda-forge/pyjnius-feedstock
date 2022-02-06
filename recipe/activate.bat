d11264ba70cbced2ded324bdcbcacc726ff2652c
H. Vetinari <h.vetinari@gmx.com>
2022-02-06 13:29:08 +1100

    more debug

--- recipe/activate.bat
+++ recipe/activate.bat
@@ -1,8 +1,10 @@
 echo "HELLO FROM ACTIVATE.BAT"
 if not "%CLASSPATH%"=="" (
     REM only set backup if CLASSPATH is non-empty
+    echo "non-empty CLASSPATH"
     set "CONDA_CLASSPATH_BACKUP=%CLASSPATH%"
     set "CLASSPATH=%CLASSPATH%;%PREFIX%\share\pyjnius\pyjnius.jar"
 ) else (
+    echo "empty CLASSPATH"
     set "CLASSPATH=%PREFIX%\share\pyjnius\pyjnius.jar"
 )
--- recipe/activate.sh
+++ recipe/activate.sh
@@ -2,8 +2,10 @@ echo "HELLO FROM ACTIVATE.SH"
 if [[ -n "${CLASSPATH}" ]]
 then
     # only set backup if CLASSPATH is non-empty
+    echo "non-empty CLASSPATH"
     export CONDA_CLASSPATH_BACKUP="${CLASSPATH}"
     export CLASSPATH="${CLASSPATH}:${CONDA_PREFIX}/share/pyjnius/pyjnius.jar"
 else
+    echo "empty CLASSPATH"
     export CLASSPATH="${CONDA_PREFIX}/share/pyjnius/pyjnius.jar"
 fi
